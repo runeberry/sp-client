6353b56fc486e877bfe93fcbb95f1fabd104743e
dolphinspired <ultima749@gmail.com>
2022-01-18 20:23:24 -0500

    Add index.html

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,3 +1,4 @@
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const path = require('path');
 
 module.exports = {
@@ -18,4 +19,11 @@ module.exports = {
     filename: 'bundle.min.js',
     path: path.resolve(__dirname, '../dist'),
   },
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'site', to: './' },
+      ],
+    }),
+  ],
 };
